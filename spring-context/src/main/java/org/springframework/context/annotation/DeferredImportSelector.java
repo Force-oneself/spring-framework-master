2416cb423a508dde4c2f31b87244fb0c6d712314
ForceOneself <1044749290@qq.com>
2021-09-03 17:55:49 +0800

    add(spring-bean)： DeferredImportSelector 注释补充

--- spring-context/src/main/java/org/springframework/context/annotation/DeferredImportSelector.java
+++ spring-context/src/main/java/org/springframework/context/annotation/DeferredImportSelector.java
@@ -20,16 +20,16 @@ import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.lang.Nullable;
 
 /**
- * A variation of {@link ImportSelector} that runs after all {@code @Configuration} beans
- * have been processed. This type of selector can be particularly useful when the selected
- * imports are {@code @Conditional}.
+ * 在所有 {@code @Configuration} bean 之后运行的 {@link ImportSelector} 变体
+ * 已经处理。这种类型的选择器在选定时特别有用
+ * 进口是 {@code @Conditional}。
  *
- * <p>Implementations can also extend the {@link org.springframework.core.Ordered}
- * interface or use the {@link org.springframework.core.annotation.Order} annotation to
- * indicate a precedence against other {@link DeferredImportSelector DeferredImportSelectors}.
+ * <p>实现也可以扩展 {@link org.springframework.core.Ordered}
+ * 接口或使用 {@link org.springframework.core.annotation.Order} 注释来
+ * 表示优先于其他 {@link DeferredImportSelector DeferredImportSelectors}。
  *
- * <p>Implementations may also provide an {@link #getImportGroup() import group} which
- * can provide additional sorting and filtering logic across different selectors.
+ * <p>实现还可以提供一个 {@link getImportGroup() 导入组}
+ * 可以在不同的选择器之间提供额外的排序和过滤逻辑。
  *
  * @author Phillip Webb
  * @author Stephane Nicoll
@@ -37,89 +37,89 @@ import org.springframework.lang.Nullable;
  */
 public interface DeferredImportSelector extends ImportSelector {
 
-	/**
-	 * Return a specific import group.
-	 * <p>The default implementations return {@code null} for no grouping required.
-	 * @return the import group class, or {@code null} if none
-	 * @since 5.0
-	 */
-	@Nullable
-	default Class<? extends Group> getImportGroup() {
-		return null;
-	}
+    /**
+     * R返回特定的导入组。 <p>默认实现返回 {@code null} 不需要分组。
+     *
+     * @return the import group class, or {@code null} if none
+     * @since 5.0
+     */
+    @Nullable
+    default Class<? extends Group> getImportGroup() {
+        return null;
+    }
 
 
-	/**
-	 * Interface used to group results from different import selectors.
-	 * @since 5.0
-	 */
-	interface Group {
+    /**
+     * 用于对来自不同导入选择器的结果进行分组的接口。
+     *
+     * @since 5.0
+     */
+    interface Group {
 
-		/**
-		 * Process the {@link AnnotationMetadata} of the importing @{@link Configuration}
-		 * class using the specified {@link DeferredImportSelector}.
-		 */
-		void process(AnnotationMetadata metadata, DeferredImportSelector selector);
+        /**
+         * Process the {@link AnnotationMetadata} of the importing @{@link Configuration}
+         * class using the specified {@link DeferredImportSelector}.
+         */
+        void process(AnnotationMetadata metadata, DeferredImportSelector selector);
 
-		/**
-		 * Return the {@link Entry entries} of which class(es) should be imported
-		 * for this group.
-		 */
-		Iterable<Entry> selectImports();
+        /**
+         * 返回应导入哪个类的 {@link Entry 条目}对于这个组。
+         */
+        Iterable<Entry> selectImports();
 
 
-		/**
-		 * An entry that holds the {@link AnnotationMetadata} of the importing
-		 * {@link Configuration} class and the class name to import.
-		 */
-		class Entry {
+        /**
+         * 包含导入的 {@link AnnotationMetadata} 的条目
+         * {@link Configuration} class and the class name to import.
+         */
+        class Entry {
 
-			private final AnnotationMetadata metadata;
+            private final AnnotationMetadata metadata;
 
-			private final String importClassName;
+            private final String importClassName;
 
-			public Entry(AnnotationMetadata metadata, String importClassName) {
-				this.metadata = metadata;
-				this.importClassName = importClassName;
-			}
+            public Entry(AnnotationMetadata metadata, String importClassName) {
+                this.metadata = metadata;
+                this.importClassName = importClassName;
+            }
 
-			/**
-			 * Return the {@link AnnotationMetadata} of the importing
-			 * {@link Configuration} class.
-			 */
-			public AnnotationMetadata getMetadata() {
-				return this.metadata;
-			}
+            /**
+             * Return the {@link AnnotationMetadata} of the importing
+             * {@link Configuration} class.
+             */
+            public AnnotationMetadata getMetadata() {
+                return this.metadata;
+            }
 
-			/**
-			 * Return the fully qualified name of the class to import.
-			 */
-			public String getImportClassName() {
-				return this.importClassName;
-			}
+            /**
+             * Return the fully qualified name of the class to import.
+             */
+            public String getImportClassName() {
+                return this.importClassName;
+            }
 
-			@Override
-			public boolean equals(@Nullable Object other) {
-				if (this == other) {
-					return true;
-				}
-				if (other == null || getClass() != other.getClass()) {
-					return false;
-				}
-				Entry entry = (Entry) other;
-				return (this.metadata.equals(entry.metadata) && this.importClassName.equals(entry.importClassName));
-			}
+            @Override
+            public boolean equals(@Nullable Object other) {
+                if (this == other) {
+                    return true;
+                }
+                if (other == null || getClass() != other.getClass()) {
+                    return false;
+                }
+                Entry entry = (Entry) other;
+                return (this.metadata.equals(entry.metadata) && this.importClassName.equals(entry.importClassName));
+            }
 
-			@Override
-			public int hashCode() {
-				return (this.metadata.hashCode() * 31 + this.importClassName.hashCode());
-			}
+            @Override
+            public int hashCode() {
+                return (this.metadata.hashCode() * 31 + this.importClassName.hashCode());
+            }
 
-			@Override
-			public String toString() {
-				return this.importClassName;
-			}
-		}
-	}
+            @Override
+            public String toString() {
+                return this.importClassName;
+            }
+        }
+    }
 
 }
--- spring-context/src/main/java/org/springframework/context/annotation/ImportBeanDefinitionRegistrar.java
+++ spring-context/src/main/java/org/springframework/context/annotation/ImportBeanDefinitionRegistrar.java
@@ -53,50 +53,52 @@ import org.springframework.core.type.AnnotationMetadata;
  *
  * @author Chris Beams
  * @author Juergen Hoeller
- * @since 3.1
  * @see Import
  * @see ImportSelector
  * @see Configuration
+ * @since 3.1
  */
 public interface ImportBeanDefinitionRegistrar {
 
-	/**
-	 * Register bean definitions as necessary based on the given annotation metadata of
-	 * the importing {@code @Configuration} class.
-	 * <p>Note that {@link BeanDefinitionRegistryPostProcessor} types may <em>not</em> be
-	 * registered here, due to lifecycle constraints related to {@code @Configuration}
-	 * class processing.
-	 * <p>The default implementation delegates to
-	 * {@link #registerBeanDefinitions(AnnotationMetadata, BeanDefinitionRegistry)}.
-	 * @param importingClassMetadata annotation metadata of the importing class
-	 * @param registry current bean definition registry
-	 * @param importBeanNameGenerator the bean name generator strategy for imported beans:
-	 * {@link ConfigurationClassPostProcessor#IMPORT_BEAN_NAME_GENERATOR} by default, or a
-	 * user-provided one if {@link ConfigurationClassPostProcessor#setBeanNameGenerator}
-	 * has been set. In the latter case, the passed-in strategy will be the same used for
-	 * component scanning in the containing application context (otherwise, the default
-	 * component-scan naming strategy is {@link AnnotationBeanNameGenerator#INSTANCE}).
-	 * @since 5.2
-	 * @see ConfigurationClassPostProcessor#IMPORT_BEAN_NAME_GENERATOR
-	 * @see ConfigurationClassPostProcessor#setBeanNameGenerator
-	 */
-	default void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry,
-			BeanNameGenerator importBeanNameGenerator) {
+    /**
+     * R根据给定的注释元数据根据需要注册 bean 定义
+     * 导入 {@code @Configuration} 类。
+     * <p>请注意，{@link BeanDefinitionRegistryPostProcessor} 类型可能<em>不是<em>
+     * 由于与 {@code @Configuration} 相关的生命周期限制，在此处注册
+     * 类处理。
+     * <p>默认实现委托给
+     * {@link registerBeanDefinitions(AnnotationMetadata, BeanDefinitionRegistry)}。
+     *
+     * @param importingClassMetadata  annotation metadata of the importing class
+     * @param registry                current bean definition registry
+     * @param importBeanNameGenerator the bean name generator strategy for imported beans:
+     *                                {@link ConfigurationClassPostProcessor#IMPORT_BEAN_NAME_GENERATOR} by default, or a
+     *                                user-provided one if {@link ConfigurationClassPostProcessor#setBeanNameGenerator}
+     *                                has been set. In the latter case, the passed-in strategy will be the same used for
+     *                                component scanning in the containing application context (otherwise, the default
+     *                                component-scan naming strategy is {@link AnnotationBeanNameGenerator#INSTANCE}).
+     * @see ConfigurationClassPostProcessor#IMPORT_BEAN_NAME_GENERATOR
+     * @see ConfigurationClassPostProcessor#setBeanNameGenerator
+     * @since 5.2
+     */
+    default void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry,
+                                         BeanNameGenerator importBeanNameGenerator) {
 
-		registerBeanDefinitions(importingClassMetadata, registry);
-	}
+        registerBeanDefinitions(importingClassMetadata, registry);
+    }
 
-	/**
-	 * Register bean definitions as necessary based on the given annotation metadata of
-	 * the importing {@code @Configuration} class.
-	 * <p>Note that {@link BeanDefinitionRegistryPostProcessor} types may <em>not</em> be
-	 * registered here, due to lifecycle constraints related to {@code @Configuration}
-	 * class processing.
-	 * <p>The default implementation is empty.
-	 * @param importingClassMetadata annotation metadata of the importing class
-	 * @param registry current bean definition registry
-	 */
-	default void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
-	}
+    /**
+     * Register bean definitions as necessary based on the given annotation metadata of
+     * the importing {@code @Configuration} class.
+     * <p>Note that {@link BeanDefinitionRegistryPostProcessor} types may <em>not</em> be
+     * registered here, due to lifecycle constraints related to {@code @Configuration}
+     * class processing.
+     * <p>The default implementation is empty.
+     *
+     * @param importingClassMetadata annotation metadata of the importing class
+     * @param registry               current bean definition registry
+     */
+    default void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
+    }
 
 }
